69007b2d1feb51b0d8e5ca883ebe9890d80344b6
DariasKehl <neocount@gmail.com>
2020-11-18 13:15:48 -0500

    commit tree 6f9dbf855f148beb4f5a06d3f3fec1d9fd90ec3a

---
+++ lib/insert.sql
@@ -0,0 +1,36 @@
+INSERT INTO bears (
+    name, age, sex, color, temperament, alive) 
+    VALUES("Mr. Chocolate", 4, "M", "Brown", "Slovenly", 1);
+
+INSERT INTO bears (
+    name, age, sex, color, temperament, alive) 
+    VALUES("Rowdy", 11, "M", "Black", "Aggressive", 1);
+
+INSERT INTO bears (
+    name, age, sex, color, temperament, alive) 
+    VALUES("Tabitha", 6, "F", "Brown", "Calm", 1);
+
+INSERT INTO bears (
+    name, age, sex, color, temperament, alive) 
+    VALUES("Sergeant Brown", 10, "M", "Brown", "Injured, Limps", 0);
+
+INSERT INTO bears (
+    name, age, sex, color, temperament, alive) 
+    VALUES("Melissa", 21, "F", "Polar Bear", "Dangerous", 0);
+
+INSERT INTO bears (
+    name, age, sex, color, temperament, alive) 
+    VALUES("Grinch", 2, "M", "Black", "Stained with Green Dye", 1);
+
+INSERT INTO bears (
+    name, age, sex, color, temperament, alive) 
+    VALUES("Wendy", 8, "F", "Brown", "Absolute sweetheart", 1);
+
+INSERT INTO bears (
+    name, age, sex, color, temperament, alive) 
+    VALUES(null, 3, "M", "Grizzly", "Killed Tim", 1);
+
+
+
+
+
